32c7ef3a6589cb0f76aa48800edc3dd7fe5c3d81
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2022-03-21 13:02:36 +0530

    changed debug

--- btre/settings.py
+++ btre/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q4a3xbp7d88w2v$d6j^5u)7*($#8ko2zh*)e7hfh7_2snmioj9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['bt-real-estate-02.herokuapp.com', '127.0.0.1', '0.0.0.0']
